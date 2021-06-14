4ea5cd2234821c6a837f5ae937c19847bfee0a86
jbellier <49211212+jbellier@users.noreply.github.com>
2021-06-14 16:40:03 -0700

    Update GEFSv12_download_batch.py
    I have modified the batch script to adapt to the slightly modified `Download_GEFSv12_function` function. 
    `bounds` and `resol` are now specified here.
    I have added some explanations for the "user's input".
    Also, instead of specifying `ivar` in the batch script, I have added a loop over all variables. The user will be able to adapt it for whatever variables she wants to loop over.

--- GEFSv12_download_batch.py
+++ GEFSv12_download_batch.py
@@ -19,24 +19,36 @@ from Download_GEFSv12_function import Download_GEFSv12_function
 ## User inputs:
 #-----------------------------------------------------------
 #===================================================================
-outpath            = '/Volumes/Drobo2_RochelleW/Data/FireWxData/GEFSv12/025deg/perturbed/surface/3hrint_start0hr/'  #change to where you want to store the data
+
+## Change to where you want to store the data
+outpath            = '/Volumes/Drobo2_RochelleW/Data/FireWxData/GEFSv12/025deg/perturbed/surface/3hrint_start0hr/'
+
+## Name of the variables to be downloaded (must be the exact names used in AWS):
 var_download_name  = ['dswrf_sfc','ugrd_hgt','vgrd_hgt',                                      # direct predictors
                       'hgt_hybr','pres_hybr','rh_hybr','tmp_hybr','ugrd_hybr','vgrd_hybr',    # hybrid levels predictors
                       'pres_msl','ugrd_hgt','vgrd_hgt','hgt_ceiling','pwat_eatm','tcdc_eatm'] # indirect predictors
+
+## Should be None is the variable has only one level stored in the GRIB file (e.g., all the surface variables)
+##  Otherwise, you should provide the level at which the variable is desired (e.g., 100 for the 100m wind, 850 for data at 850hPa, 1 for the 1-sigma data at hybrid levels, etc.). 
+##  If a scalar is provided, the output netCDF will contain only that level. 
+##  If a list of levels is provided, the output netCDF will contain all the levels.
 optional_levels    = [None,100,100,
                       [1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4],
                       None,10,10,None,None,None]
+
+## Name of the directories under which the variables will be stored (can be different from var_download_name):
 var_directory_name = ['dswrf','u100m','v100m',
                       'hgt_hybr','pres_hybr','rh_hybr','tmp_hybr','ugrd_hybr','vgrd_hybr',
                       'pres_msl','u10m','v10m','hgt_ceiling','pwat','tcc']
 
+## Reslution of the forecasts: (choose between 'Days:1-10' and 'Days:10-16')
+resol = 'Days:1-10'
+#===================================================================
 
-#Change index for the variable you want to download. ivar = 3 will download 'apcp' data
-ivar = 4
 
 
-#---The rest of the code shouldn't need to be changed---
-
+## Bounds of the sub-area to keep the data over:
+bounds = [23.0, 51.0, -131, -63.0] # CONUS DOMAIN for the renewables project
 
 #reforecasts GEFSv12: 01/01/2000 to 12/31/2019
 dateStart     = '2000-01-01 00:00:00'
@@ -55,14 +67,15 @@ while date <= end_date:
 #------------------------------------------------------------------------------------------------------------------------ 
 #Download data from AWS server for every date in 'list_of_dates' by calling download function.
 
-for idate in range(len(list_of_dates)):
-    date_from = list_of_dates[idate]
-    date_to   = date_from
+for ivar in range(len(var_download_name)):
+    for idate in range(len(list_of_dates)):
+        date_from = list_of_dates[idate]
+        date_to   = date_from
 
-    try:
-        Download_GEFSv12_function(outpath,var_directory_name[ivar],var_download_name[ivar],optional_levels[ivar],date_from,date_to)
-    except FileNotFoundError as err:
-        print("FileNotFoundError: {0}".format(err))
-        continue  #try next date in the list
+        try:
+            Download_GEFSv12_function(outpath,var_directory_name[ivar],var_download_name[ivar],optional_levels[ivar],resol,date_from,date_to,bounds)
+        except FileNotFoundError as err:
+            print("FileNotFoundError: {0}".format(err))
+            continue  #try next date in the list, but doesn't stop the script
 
-#When you're done downloading, you should have 7305 files for Days1-10 (and 7305 files for Days10-16 if you requested those) for each variable!
+# When you're done downloading, you should have 7305 files for Days1-10 (and 7305 files for Days10-16 if you requested those) for each variable!
